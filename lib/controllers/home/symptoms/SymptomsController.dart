a588d8a1e3eb4b3ba155301e0f595680a7ffb847
Minarul Ali <minarul@technoexponent.com>
2021-08-16 12:23:07 +0530

    Init Symtomps

--- lib/controllers/home/symptoms/SymptomsController.dart
+++ lib/controllers/home/symptoms/SymptomsController.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_ibs/Store/HiveStore.dart';
+import 'package:flutter_ibs/services/ServiceApi.dart';
+import 'package:flutter_ibs/utils/SnackBar.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 
@@ -16,6 +19,12 @@ class SymptomsController extends GetxController {
   @override
   void onInit() {
     super.onInit();
+    getSymptoms();
     formattedTime = int.parse(DateFormat('kk').format(now.value)).obs;
+  }
+  getSymptoms()async{
+    var loginId = await HiveStore().get("LOGINID");
+    final data = await ServiceApi().getSymptomsApi(bodyData: {"loginid": loginId});
+    print("Data: $data");
   }
 }
