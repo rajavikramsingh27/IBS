7d9cebb331be7abbb8adcf71a76b73e57ad83178
Minarul Ali <minarul@technoexponent.com>
2021-09-06 20:40:07 +0530

    Sending Models Updated

--- lib/controllers/health/HealthController.dart
+++ lib/controllers/health/HealthController.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_ibs/controllers/signup/SignUpController.dart';
+import 'package:flutter_ibs/models/HealthWellnessModel/HealthWellnessModel.dart';
 import 'package:flutter_ibs/models/TrackablesListModel/TrackablesListModel.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
@@ -11,7 +13,9 @@ class HealthController extends GetxController {
   RxBool loader = false.obs;
   List<String> timeList = ["01:00","02:00","03:00","04:00","05:00","06:00","07:00","08:00","09:00","10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00","18:00","19:00","20:00","21:00","22:00","23:00","24:00",];
   RxString selectedTime = "01:00".obs;
-  RxBool switchValue = true.obs;
+  RxBool switchValue = false.obs;
+  Rx<HealthWellnessModel> healthWellnessModel = HealthWellnessModel().obs;
+  SignUpController _signUpController = Get.find();
 
   onTapped(int index) async {
     currentIndex.value = index;
@@ -23,8 +27,45 @@ class HealthController extends GetxController {
     formattedTime = int.parse(DateFormat('kk').format(now.value)).obs;
   }
   void onSave(){
+    if (healthWellnessModel.value.items == null) {
+      healthWellnessModel.value.items = [];
+    }
+    Item item = Item(
+        tid: _signUpController.healthWellness.value.items.last.tid,
+        kind: _signUpController.healthWellness.value.items.last.kind,
+        dtype: "str",
+        value: ItemValue(str: noteTextController.text));
+    healthWellnessModel.value.items.add(item);
+    healthWellnessModel.refresh();
+    print("DATA Model : ${healthWellnessModel.toJson()}");
 
   }
 
-  void onOptionTapped({ListOption model}) {}
+  void onOptionTapped({ListOption model}) {
+    model.optionDefault = !model.optionDefault;
+
+    if (healthWellnessModel.value.items == null) {
+      healthWellnessModel.value.items = [];
+    }
+    if(switchValue.value){
+      Item item = Item(
+          tid: _signUpController.healthWellness.value.items.last.tid,
+          kind: _signUpController.healthWellness.value.items.last.kind,
+          dtype: "str",
+          value: ItemValue(str: noteTextController.text));
+      healthWellnessModel.value.items.add(item);
+    }
+
+    /*if (model.optionDefault) {
+      if (!modelValue.contains(model.value)) {
+        modelValue.add(model.value);
+      }
+    } else {
+      if (modelValue.contains(model.value)) {
+        modelValue.remove(model.value);
+      }
+    }*/
+    _signUpController.healthWellness.refresh();
+    healthWellnessModel.refresh();
+  }
 }
