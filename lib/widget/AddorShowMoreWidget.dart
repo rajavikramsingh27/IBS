3f68189a38f20b4f49c0fd34a696c0bbf64b4d69
Shubham <shubham.shukla@technoexponent.com>
2021-09-10 15:14:45 +0530

    Added_Custom_Widget

---
+++ lib/widget/AddorShowMoreWidget.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ibs/utils/Colors.dart';
+import 'package:flutter_ibs/utils/ScreenConstants.dart';
+import 'package:flutter_ibs/utils/TextStyles.dart';
+
+class AddorShowMoreWidget extends StatelessWidget {
+  final String text;
+  final Function() onTap;
+
+  const AddorShowMoreWidget({Key key, this.text, this.onTap}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: ScreenConstant.defaultWidthTen * 1.5,
+            backgroundColor: AppColors.colorArrowButton,
+            child: Icon(
+              Icons.add,
+              size: FontSize.s11,
+              color: Colors.white,
+            ),
+          ),
+          SizedBox(width: ScreenConstant.sizeDefault),
+          Text(
+            text,
+            style: TextStyles.textStyleRegular.apply(color: AppColors.white),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widget/ReminderPlanWidget.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_ibs/utils/Colors.dart';
+import 'package:flutter_ibs/utils/ScreenConstants.dart';
+import 'package:flutter_ibs/utils/TextStyles.dart';
+import 'package:flutter_ibs/widget/CustomSwitch.dart';
+
+class ReminderPlanWidget extends StatelessWidget {
+  final String title;
+  final String childText;
+  final bool valueReminder;
+  final bool valueChild;
+  final Function() onPressed;
+
+  final Function(bool) onChanged;
+  final Function(bool) onChangedChild;
+
+  final String editText;
+
+  const ReminderPlanWidget({
+    Key key,
+    this.title,
+    this.childText,
+    this.valueReminder,
+    this.valueChild,
+    this.onChanged,
+    this.onChangedChild,
+    this.editText,
+    this.onPressed,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      SizedBox(height: ScreenConstant.defaultHeightTwenty * 1.5),
+      Card(
+          margin: EdgeInsets.zero,
+          color: AppColors.colorBackground,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+          child: Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: ScreenConstant.defaultWidthTwenty,
+              ),
+              child: Column(
+                children: [
+                  SizedBox(height: ScreenConstant.defaultHeightForty),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(title,
+                          style: TextStyles.textStyleIntroDescription
+                              .apply(color: Colors.white, fontSizeDelta: -2)),
+                      CustomSwitch(
+                        value: valueReminder,
+                        color: AppColors.colorYesButton,
+                        onChanged: onChanged,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: ScreenConstant.defaultHeightTen),
+                  Divider(
+                      thickness: 1, color: AppColors.white.withOpacity(0.12)),
+                  _buildTimeList(),
+                  SizedBox(height: ScreenConstant.defaultHeightTwentyFour),
+                ],
+              )))
+    ]);
+  }
+
+  _buildTimeList() {
+    return ListView.separated(
+      itemCount: 2,
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemBuilder: (BuildContext context, int index) {
+        return Row(
+          children: [
+            Icon(
+              Icons.access_time_filled,
+              color: AppColors.colorIcons,
+            ),
+            SizedBox(width: ScreenConstant.sizeDefault),
+            Text(
+              childText,
+              style: TextStyles.textStyleRegular.apply(color: Colors.white),
+            ),
+            TextButton(
+                onPressed: onPressed,
+                child: Text(
+                  editText,
+                  style: TextStyles.textStyleRegular.apply(
+                      color: AppColors.colorSkipButton, fontSizeDelta: -2),
+                )),
+            Spacer(),
+            CustomSwitch(
+              color: AppColors.colorIcons,
+              value: valueChild,
+              onChanged: onChangedChild,
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) =>
+          Divider(thickness: 1, color: AppColors.white.withOpacity(0.12)),
+    );
+  }
+}
