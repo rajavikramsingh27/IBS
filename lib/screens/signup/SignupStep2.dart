160d3b29fb8d1f9d2787b4d7b4f25875bcf21ab6
Ian Wood <ian@weareike.com>
2021-09-11 15:45:31 -0400

    Trackable List View uses recursion to walk down item.child[x].items[y]
    The original list was not parsing the children correctly.

--- lib/screens/signup/SignupStep2.dart
+++ lib/screens/signup/SignupStep2.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ibs/controllers/signup/SignUpController.dart';
+import 'package:flutter_ibs/models/TrackablesListModel/TrackablesListModel.dart';
 import 'package:flutter_ibs/routes/RouteConstants.dart';
 import 'package:flutter_ibs/utils/Assets.dart';
 import 'package:flutter_ibs/utils/Colors.dart';
@@ -64,6 +65,7 @@ class SignupStep2 extends StatelessWidget {
               )));
   }
 
+  /// Builds the Graphic Title Block
   _buildTrackingOptions() {
     return Stack(
       children: [
@@ -108,6 +110,11 @@ class SignupStep2 extends StatelessWidget {
     // );
   }
 
+  /// Builds the list of all trackables.
+  /// Creates the top-level category toggle widgets
+  /// Calls supporting functions to build the nested set of children.
+  /// The nested functions, _renderSubItem and _renderChildren
+  /// get called recursively to walk down the tree.
   _buildTrackingList() {
     return ListView.builder(
       shrinkWrap: true,
@@ -117,146 +124,117 @@ class SignupStep2 extends StatelessWidget {
         var model = _controller.trackList.value.data[index];
 
         return Theme(
-          data: Get.theme.copyWith(dividerColor: Colors.transparent),
-          child: CustomExpansionTile(
-              tilePadding: EdgeInsets.zero,
-              onExpansionChanged: (isExpanding) {},
-              initiallyExpanded: true,
-              title: Container(
-                decoration: BoxDecoration(
-                    color: AppColors.colorBackground,
-                    borderRadius: BorderRadius.circular(16)),
-                child: Row(
-                  children: [
-                    CustomCheckBox(
-                      checkedFillColor: AppColors.colorYesButton,
-                      value: _controller.trackList.value.data[index].enabled,
-                      onChanged: (val) {
-                        _controller.trackList.value.data[index].enabled =
-                            !_controller.trackList.value.data[index].enabled;
-                        _controller.trackList.refresh();
-                      },
-                    ),
-                    Text("${_controller.trackList.value.data[index].name}".tr,
-                        style: TextStyles.textStyleIntroDescription
-                            .apply(color: Colors.white, fontSizeDelta: -3)),
-                    Spacer(),
-                    Icon(
-                      Icons.keyboard_arrow_down_outlined,
-                      color: Colors.white,
-                    ),
-                    SizedBox(width: ScreenConstant.defaultWidthTwenty)
-                  ],
-                ),
-              ),
-              children: [
-                Container(
+            data: Get.theme.copyWith(dividerColor: Colors.transparent),
+            child: CustomExpansionTile(
+                tilePadding: EdgeInsets.zero,
+                onExpansionChanged: (isExpanding) {},
+                initiallyExpanded: true,
+                title: Container(
                   decoration: BoxDecoration(
-                      color: Colors.grey[50],
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(16),
-                          bottomRight: Radius.circular(16))),
-                  child: ListView.builder(
-                    physics: NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount:
-                        _controller.trackList.value.data[index].items.length,
-                    itemBuilder: (BuildContext context, int idx) {
-                      var subModel =
-                          _controller.trackList.value.data[index].items[idx];
-                      return Visibility(
-                        visible: subModel.isVisible,
-                        child: CustomExpansionTile(
-                          tilePadding: EdgeInsets.zero,
-                          onExpansionChanged: (isExpanding) {},
-                          initiallyExpanded: true,
-                          title: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              CustomCheckBox(
-                                /*value: model.enabled == false
-                                    ? false
-                                    : _controller.trackList.value.data[index]
-                                        .items[idx].enabledDefault,*/
-                                value: subModel.enabledDefault,
-                          onChanged: (val) {
-                                  _controller.trackList.value.data[index]
-                                          .items[idx].enabledDefault =
-                                      !_controller.trackList.value.data[index]
-                                          .items[idx].enabledDefault;
-                                  _controller.trackList.refresh();
-                                },
-                              ),
-                              Expanded(
-                                child: InkWell(
-                                    onTap: () {},
-                                    child: Text(
-                                      "${subModel.name}".tr,
-                                      style: TextStyles.textStyleRegular
-                                          .apply(color: Colors.black),
-                                      maxLines: 1,
-                                      textAlign: TextAlign.left,
-                                      overflow: TextOverflow.ellipsis,
-                                    )),
-                              )
-                            ],
-                          ),
-                          children: [
-                            subModel?.children?.isEmpty ?? true
-                                ? Offstage()
-                                : ListView.builder(
-                                    physics: NeverScrollableScrollPhysics(),
-                                    padding: EdgeInsets.only(
-                                        left: ScreenConstant.sizeExtraLarge),
-                                    shrinkWrap: true,
-                                    itemCount:
-                                        subModel.children.last.items.length,
-                                    itemBuilder: (BuildContext context, int idx) {
-                                      var subModelChild =
-                                          subModel.children.last.items[idx];
-
-                                      return Visibility(
-                                        visible: subModelChild.isVisible ,
-                                        child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.start,
-                                          children: [
-                                            CustomCheckBox(
-                                              value: subModelChild.enabledDefault,/* ==
-                                                          false ||
-                                                      model.enabled == false
-                                                  ? false
-                                                  : subModelChild.enabledDefault ??
-                                                      true,*/
-                                              onChanged: (val) {
-                                                subModelChild.enabledDefault =
-                                                    !subModelChild.enabledDefault;
-                                                _controller.trackList.refresh();
-                                              },
-                                            ),
-                                            Expanded(
-                                              child: InkWell(
-                                                  onTap: () {},
-                                                  child: Text(
-                                                    "${subModelChild.name}".tr,
-                                                    maxLines: 1,
-                                                    textAlign: TextAlign.left,
-                                                    overflow: TextOverflow.ellipsis,
-                                                  )),
-                                            )
-                                          ],
-                                        ),
-                                      );
-                                    })
-                          ],
-                        ),
-                      );
-                    },
+                      color: AppColors.colorBackground,
+                      borderRadius: BorderRadius.circular(16)),
+                  child: Row(
+                    children: [
+                      CustomCheckBox(
+                        checkedFillColor: AppColors.colorYesButton,
+                        value: model.enabled,
+                        onChanged: (val) {
+                          model.enabled = !model.enabled;
+                          _controller.trackList.refresh();
+                        },
+                      ),
+                      Text("${_controller.trackList.value.data[index].name}".tr,
+                          style: TextStyles.textStyleIntroDescription
+                              .apply(color: Colors.white, fontSizeDelta: -3)),
+                      Spacer(),
+                      Icon(
+                        Icons.keyboard_arrow_down_outlined,
+                        color: Colors.white,
+                      ),
+                      SizedBox(width: ScreenConstant.defaultWidthTwenty)
+                    ],
                   ),
                 ),
-              ]),
-        );
+                children: [
+                  Container(
+                      decoration: BoxDecoration(
+                          color: Colors.grey[50],
+                          borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(16),
+                              bottomRight: Radius.circular(16))),
+                      child: ListView.builder(
+                          physics: NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: _controller
+                              .trackList.value.data[index].items.length,
+                          itemBuilder: (BuildContext context, int idx) {
+                            return _renderSubItem(model.items[idx]);
+                          })),
+                ]));
       },
     );
+  }
+
+  /// Renders a specific TrackableItem
+  /// This items children will be passed to
+  /// _renderChildren for rendering.
+  _renderSubItem(TrackableItem item) {
+    return Visibility(
+        visible: item.isVisible,
+        child: CustomExpansionTile(
+            tilePadding: EdgeInsets.zero,
+            onExpansionChanged: (isExpanding) {},
+            initiallyExpanded: true,
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                CustomCheckBox(
+                  value: item.enabledDefault,
+                  onChanged: (val) {
+                    item.enabledDefault = !item.enabledDefault;
+                    _controller.trackList.refresh();
+                  },
+                ),
+                Expanded(
+                  child: InkWell(
+                      onTap: () {},
+                      child: Text(
+                        "${item.name}".tr,
+                        style: TextStyles.textStyleRegular
+                            .apply(color: Colors.black),
+                        maxLines: 1,
+                        textAlign: TextAlign.left,
+                        overflow: TextOverflow.ellipsis,
+                      )),
+                )
+              ],
+            ),
+            children: _renderChildren(item.children)
+        )
+    );
+  }
+
+  /// Iterates over a list of TrackableChild objects
+  /// passing the child.items back to _renderSubItem
+  /// (which may in tern call _renderChildren on those
+  /// items to walk down the tree)
+  _renderChildren(List<TrackableChild> children) {
+    List<Widget> widgets = [];
+    children.forEach( (child) {
+      var listView = (ListView.builder(
+          physics: NeverScrollableScrollPhysics(),
+          shrinkWrap: true,
+          padding: EdgeInsets.only(
+              left: ScreenConstant.sizeExtraLarge),
+          itemCount:
+          child.items.length,
+          itemBuilder: (BuildContext context, int idx) {
+           // print ("Rendering child item: " + child.items[idx].tid + ", " + child.items[idx].isVisible.toString());
+            return _renderSubItem(child.items[idx]);
+          }
+      ));
+      widgets.add(listView);
+    });
+    return widgets;
   }
 }
