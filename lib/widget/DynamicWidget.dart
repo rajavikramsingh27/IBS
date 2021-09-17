9235d0915679ca1e168ed0b874b4e63bcbb13378
Minarul Ali <minarul@technoexponent.com>
2021-09-17 15:29:35 +0530

    Fixed stateManagment

--- lib/widget/DynamicWidget.dart
+++ lib/widget/DynamicWidget.dart
@@ -151,6 +151,7 @@ class _DynamicWidgetState extends State<DynamicWidget> {
                           setState(() {
                             widget.data[index].select.selectDefault =
                                 optionItem;
+                            _treatmentPlanController.selectedDay.value = optionItem.label;
                           });
                         },
                         isEnabled: true,
