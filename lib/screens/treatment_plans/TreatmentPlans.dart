ff213f9bff6453dcf1eea720fff6e93d6b5af5f9
Shubham <shubham.shukla@technoexponent.com>
2021-09-28 13:08:48 +0530

    bottom_bar_added

--- lib/screens/treatment_plans/TreatmentPlans.dart
+++ lib/screens/treatment_plans/TreatmentPlans.dart
@@ -4,6 +4,7 @@ import 'package:flutter_ibs/utils/Assets.dart';
 import 'package:flutter_ibs/utils/Colors.dart';
 import 'package:flutter_ibs/utils/ScreenConstants.dart';
 import 'package:flutter_ibs/utils/TextStyles.dart';
+import 'package:flutter_ibs/widget/CustomBottomNavigation.dart';
 import 'package:flutter_ibs/widget/Indicator.dart';
 import 'package:flutter_ibs/widget/LeadingBackButton.dart';
 import 'package:flutter_ibs/widget/TreatmentPlanListItem.dart';
@@ -44,86 +45,96 @@ class TreatmentPlans extends StatelessWidget {
       ),
       body: Obx(() => _treatmentPlanController.loader.value
           ? Center(child: CircularProgressIndicator())
-          : ListView(
-              padding: ScreenConstant.spacingAllMedium,
-              physics: ClampingScrollPhysics(),
+          : Stack(
               children: [
-                Center(
-                    child: Image.asset(Assets.treatment,
-                        width: ScreenConstant.defaultHeightTwoHundredTen)),
-                SizedBox(height: ScreenConstant.defaultHeightSixteen),
-                Padding(
-                  padding: EdgeInsets.symmetric(
-                      horizontal: ScreenConstant.defaultWidthTwenty * 2),
-                  child: Text(
-                    "There is no cure for IBS but several treatments exist to help you manage your symptoms.",
-                    textAlign: TextAlign.center,
-                    style:
-                        TextStyles.textStyleRegular.apply(color: Colors.black),
-                  ),
-                ),
-                SizedBox(height: ScreenConstant.defaultHeightSixteen),
-                Indicator(
-                  controller: _treatmentPlanController.pageController,
-                  itemCount: 4,
-                ),
-                SizedBox(height: ScreenConstant.defaultHeightTwentyFour),
-                ListView.builder(
-                  shrinkWrap: true,
+                ListView(
+                  padding: ScreenConstant.spacingAllMedium,
                   physics: ClampingScrollPhysics(),
-                  itemCount: _treatmentPlanController.listCategory.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          _treatmentPlanController.listCategory
-                              .toList()[index]
-                              .tr,
-                          style: TextStyles.textStyleIntroDescription
-                              .apply(color: Colors.black, fontSizeDelta: -4),
-                          textAlign: TextAlign.start,
-                        ),
-                        SizedBox(height: ScreenConstant.defaultHeightTen),
-                        ListView.builder(
-                            shrinkWrap: true,
-                            physics: ClampingScrollPhysics(),
-                            itemCount: _treatmentPlanController
-                                .treatmentPlanItemData.length,
-                            itemBuilder: (BuildContext context, int ind) {
-                              if (_treatmentPlanController.listCategory
-                                      .toList()[index] ==
-                                  _treatmentPlanController
-                                      .treatmentPlanItemData[ind]
-                                      .planCategory) {
-                                return Column(
-                                  children: [
-                                    TreatmentPlanListItem(
-                                        title: _treatmentPlanController
-                                            .treatmentPlanItemData[ind]
-                                            .planName
-                                            .tr,
-                                      onPressed: () {_treatmentPlanController
-                                          .toTreatmentPlanListWidget(
-                                              data: _treatmentPlanController
-                                                  .treatmentPlanItemData[ind]);
-                                    },
-                                      tid: _treatmentPlanController
+                  children: [
+                    Center(
+                        child: Image.asset(Assets.treatment,
+                            width: ScreenConstant.defaultHeightTwoHundredTen)),
+                    SizedBox(height: ScreenConstant.defaultHeightSixteen),
+                    Padding(
+                      padding: EdgeInsets.symmetric(
+                          horizontal: ScreenConstant.defaultWidthTwenty * 2),
+                      child: Text(
+                        "There is no cure for IBS but several treatments exist to help you manage your symptoms.",
+                        textAlign: TextAlign.center,
+                        style: TextStyles.textStyleRegular
+                            .apply(color: Colors.black),
+                      ),
+                    ),
+                    SizedBox(height: ScreenConstant.defaultHeightSixteen),
+                    Indicator(
+                      controller: _treatmentPlanController.pageController,
+                      itemCount: 4,
+                    ),
+                    SizedBox(height: ScreenConstant.defaultHeightTwentyFour),
+                    ListView.builder(
+                      shrinkWrap: true,
+                      physics: ClampingScrollPhysics(),
+                      itemCount: _treatmentPlanController.listCategory.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              _treatmentPlanController.listCategory
+                                  .toList()[index]
+                                  .tr,
+                              style: TextStyles.textStyleIntroDescription.apply(
+                                  color: Colors.black, fontSizeDelta: -4),
+                              textAlign: TextAlign.start,
+                            ),
+                            SizedBox(height: ScreenConstant.defaultHeightTen),
+                            ListView.builder(
+                                shrinkWrap: true,
+                                physics: ClampingScrollPhysics(),
+                                itemCount: _treatmentPlanController
+                                    .treatmentPlanItemData.length,
+                                itemBuilder: (BuildContext context, int ind) {
+                                  if (_treatmentPlanController.listCategory
+                                          .toList()[index] ==
+                                      _treatmentPlanController
                                           .treatmentPlanItemData[ind]
-                                          .pid,
-                                    ),
-                                    SizedBox(
-                                        height: ScreenConstant.sizeDefault),
-                                  ],
-                                );
-                              } else
-                                return Offstage();
-                            }),
-                      ],
-                    );
-                  },
+                                          .planCategory) {
+                                    return Column(
+                                      children: [
+                                        TreatmentPlanListItem(
+                                          title: _treatmentPlanController
+                                              .treatmentPlanItemData[ind]
+                                              .planName
+                                              .tr,
+                                          onPressed: () {
+                                            _treatmentPlanController
+                                                .toTreatmentPlanListWidget(
+                                                    data: _treatmentPlanController
+                                                            .treatmentPlanItemData[
+                                                        ind]);
+                                          },
+                                          tid: _treatmentPlanController
+                                              .treatmentPlanItemData[ind].pid,
+                                        ),
+                                        SizedBox(
+                                            height: ScreenConstant.sizeDefault),
+                                      ],
+                                    );
+                                  } else
+                                    return Offstage();
+                                }),
+                          ],
+                        );
+                      },
+                    ),
+                    SizedBox(height: ScreenConstant.defaultHeightSeventy),
+                  ],
                 ),
-                SizedBox(height: ScreenConstant.defaultHeightTwentyFour),
+                Positioned(
+                    bottom: 0,
+                    left: 0,
+                    right: 0,
+                    child: CustomBottomNavigation()),
               ],
             )),
     );
